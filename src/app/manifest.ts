6f397f3fc9ac4dbda607afbecc5c57e44aaba5a8
Sagar-v4 <sagarvariya4@gmail.com>
2025-07-31 23:08:15 +0530

    fix: file path

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,25 +12,25 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: '#000000',
 		icons: [
 			{
-				src: 'icons/manifest-icon-192.png',
+				src: '/icons/manifest-icon-192.png',
 				sizes: '192x192',
 				type: 'image/png',
 				purpose: 'any',
 			},
 			{
-				src: 'icons/manifest-icon-192.maskable.png',
+				src: '/icons/manifest-icon-192.maskable.png',
 				sizes: '192x192',
 				type: 'image/png',
 				purpose: 'maskable',
 			},
 			{
-				src: 'icons/manifest-icon-512.png',
+				src: '/icons/manifest-icon-512.png',
 				sizes: '512x512',
 				type: 'image/png',
 				purpose: 'any',
 			},
 			{
-				src: 'icons/manifest-icon-512.maskable.png',
+				src: '/icons/manifest-icon-512.maskable.png',
 				sizes: '512x512',
 				type: 'image/png',
 				purpose: 'maskable',
@@ -38,13 +38,13 @@ export default function manifest(): MetadataRoute.Manifest {
 		],
 		screenshots: [
 			{
-				src: 'screenshots/light.png',
+				src: '/screenshots/light.png',
 				sizes: '1320x2868',
 				type: 'image/png',
 				label: 'Home screen - light',
 			},
 			{
-				src: 'screenshots/dark.png',
+				src: '/screenshots/dark.png',
 				sizes: '1320x2868',
 				type: 'image/png',
 				label: 'Home screen - dark',
@@ -58,25 +58,25 @@ export default function manifest(): MetadataRoute.Manifest {
 				short_name: 'Home',
 				icons: [
 					{
-						src: 'icons/manifest-icon-192.png',
+						src: '/icons/manifest-icon-192.png',
 						sizes: '192x192',
 						type: 'image/png',
 						purpose: 'any',
 					},
 					{
-						src: 'icons/manifest-icon-192.maskable.png',
+						src: '/icons/manifest-icon-192.maskable.png',
 						sizes: '192x192',
 						type: 'image/png',
 						purpose: 'maskable',
 					},
 					{
-						src: 'icons/manifest-icon-512.png',
+						src: '/icons/manifest-icon-512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any',
 					},
 					{
-						src: 'icons/manifest-icon-512.maskable.png',
+						src: '/icons/manifest-icon-512.maskable.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'maskable',
